34931533a21c18eb90459b434516f1b94a4b7b04
Evgeniy28 <azarov91@mail.ru>
2017-07-01 05:49:29 +0300

    Refactor user page

--- src/pages/user/user.js
+++ src/pages/user/user.js
@@ -1,7 +1,13 @@
+import UserCard from '@/components/user-card/user-card.vue'
+
 export default {
   computed: {
     user () {
       return this.$root.user
     }
+  },
+
+  components: {
+    UserCard
   }
 }
